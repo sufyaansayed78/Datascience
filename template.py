26a59fd29ed116ab0f2011816dca833885c5a1bd
Sufyaan Sayed <sufyaansayed78@gmail.com>
2025-11-03 19:56:41 +0000

    file created

---
+++ template.py
@@ -0,0 +1,36 @@
+import os 
+from pathlib import Path
+import logging
+logging.basicConfig(level=logging.INFO , format="[%(asctime)s]:%(message)s:")
+project_name="datascience"
+list_of_fies=[
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/configs/__init__.py",
+    f"src/{project_name}/configs/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "Dockerfile",
+    "setup.py",
+    "research/research.ipynb",
+    "template/index.html"
+]
+for filepath in list_of_fies:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory {filedir} for the file : {filename}")
+    
+    if not os.path.exists(filepath):
+        filepath.touch()
