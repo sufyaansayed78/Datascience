fe49000bf89d250a90659e8b3b144e130988d673
Sufyaan Sayed <sufyaansayed78@gmail.com>
2025-11-05 20:06:36 +0000

    Extracted csv

--- src/datascience/__init__.py
+++ src/datascience/__init__.py
@@ -6,7 +6,7 @@ log_str = "[%(asctime)s: %(levelname)s: %(module)s:%(message)s]"
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 log_dir ="logs"
 log_filepath = os.path.join(log_dir,f"Log At {timestamp}.log")
-os.makedirs(log_filepath,exist_ok=True)
+#os.makedirs(log_filepath,exist_ok=True)
 
 logging.basicConfig(
     level=logging.INFO,
