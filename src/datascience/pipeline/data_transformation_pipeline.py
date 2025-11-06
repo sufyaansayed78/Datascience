1be5e1462c7834f0d792d36f9b24cb867f80ef3c
Sufyaan Sayed <sufyaansayed78@gmail.com>
2025-11-06 19:34:59 +0000

    Data Transformation implemented

--- src/datascience/pipeline/data_transformation_pipeline.py
+++ src/datascience/pipeline/data_transformation_pipeline.py
@@ -8,11 +8,17 @@ class DataTransformationPipeline:
    def __init__(self):
      pass
 
-   def initiate_data_transformation():
+   def initiate_data_transformation(self):
      try : 
-        with open("")   
-        config = ConfigurationManager()
-        data_transformation_config = config.get_data_transformation_config()
-        data_transformation = DataTransformation()
-        data_transformation.train_test_splitting()
-        logger.info('Data Transformation Complete')
+        with open("artifacts\data_validation\status.txt",'r') as file :
+           status = file.read().split(" ")[-1]
+           if status == True : 
+              config = ConfigurationManager()
+              data_transformation_config = config.get_data_transformation_config()
+              data_transformation = DataTransformation()
+              data_transformation.train_test_splitting()
+              logger.info('Data Transformation Complete')
+           else:
+              raise Exception(" Your data schema is not valid ")
+     except Exception as e :
+        print(e)
